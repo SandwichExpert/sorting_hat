12407cf24b00d648fb6a69b8002f3859b76b781e
Xavier Crespo <xavier.crespo1@gmail.com>
2019-10-24 00:13:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import houses from './houses';
 
 function App() {
+  const [house, setHouse]=useState("");
+  
+  function handleClick(e){
+    e.preventDefault();
+    
+    let housesArray=[1,2,3,4]
+    const sortedHouse= housesArray[Math.floor(Math.random()*4)]
+    setHouse(sortedHouse)
+    if(house){
+      getColour()
+    }
+    console.log(sortedHouse)
+  }
+  
+  function getColour(){
+    if(house){
+      const selected=houses.find(h => h.id === house );
+      const color = selected.color;
+      return color;
+    }
+    else return "white"
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"  >
+      <div className="house" style={{backgroundColor:getColour()}}>
+        <button onClick={handleClick}><img src="https://fontmeme.com/permalink/191023/2fa289c871039636615aee62afd87b8d.png" alt="harry-potter-font" border="0"/></button>
+        <div className="house__content">
+        {house ? houses.filter(h=>h.id===house).map(house=>(
+          <div key={house.id}>
+            <div style={{textAlign:"center"}} className="house__content">
+          <h2 className="welcome">WELCOME TO {house.name.toUpperCase()}</h2>
+          </div>
+          <div className="house__content">
+          <img src={house.img} alt={house.name} className="houseImage"/>
+          </div>
+          </div>
+        )) : (<p>Click to find your house</p>)}
+      </div>
+      </div>
+      
     </div>
   );
 }
